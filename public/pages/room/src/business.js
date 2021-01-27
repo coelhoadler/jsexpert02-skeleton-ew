58fe0b7c91d39ceab64776e08c98d12dec76d8ad
Adler Coelho <adler.coelho.santos@icloud.com>
2021-01-26 22:37:20 -0300

    feature: fazendo a aula-02 da semana js expert

--- public/pages/room/src/business.js
+++ public/pages/room/src/business.js
@@ -1,18 +1,18 @@
 class Business {
 
-    constructor({ room, media, view, socketBuilder }) {
+    constructor({ room, media, view, socketBuilder, peerBuilder }) {
         this.room = room
         this.media = media
         this.view = view
-        
+
         this.socketBuilder = socketBuilder
-        .setOnUserConnected(this.onUserConnected())
-        .setOnUserDisconnected(this.onUserDisconnected())
-        .build()
+        this.peerBuilder = peerBuilder
 
-        this.socketBuilder.emit('join-room', this.room, 'xuxa da silva')
-
+        this.socket = {}
         this.currentStream = {}
+        this.currentPeer = {}
+
+        this.peers = new Map()
     }
 
     static inicialize(deps) {
@@ -21,7 +21,20 @@ class Business {
     }
 
     async _init() {
-        this.currentStream = await this.media.getCamera();
+        this.currentStream = await this.media.getCamera(true);
+        this.socket = this.socketBuilder
+            .setOnUserConnected(this.onUserConnected())
+            .setOnUserDisconnected(this.onUserDisconnected())
+            .build()
+
+        this.currentPeer = await this.peerBuilder
+            .setOnError(this.onPeerError())
+            .setOnConnectionOpened(this.onPeerConnectionOpened())
+            .setOnCallReceived(this.onCallReceived())
+            .setOnPeerStreamReceived(this.onPeerStreamReceived())
+            .build()
+
+
         this.addVideoStream('eu.adler')
     }
 
@@ -34,16 +47,49 @@ class Business {
         })
     }
 
-    onUserConnected = function() {
+    onUserConnected = function () {
         return userId => {
             console.log('user connected', userId)
+            this.currentPeer.call(userId, this.currentStream)
         }
     }
 
-    onUserDisconnected = function() {
+    onUserDisconnected = function () {
         return userId => {
             console.log('user disconnected', userId)
         }
-    }    
+    }
+
+    onPeerError = function () {
+        return error => {
+            console.error('error on peer!', error)
+        }
+    }
+
+    onPeerConnectionOpened = function () {
+        return peer => {
+            const id = peer.id
+            console.log('o peer', peer)
+            this.socket.emit('join-room', this.room, id)
+        }
+    }
+
+    onCallReceived = function () {
+        return call => {
+            console.log('answering call', call)
+            call.answer(this.currentStream)
+        }
+    }
+
+    onPeerStreamReceived = function () {
+        return (call, stream) => {
+            const callerId = call.peer
+            if (!this.peers.has(callerId)) {
+                this.addVideoStream(callerId, stream)
+                this.peers.set(callerId, { call });
+                this.view.setParticipants(this.peers.size)
+            }
+        }
+    }
 
 }
--- public/pages/room/src/view.js
+++ public/pages/room/src/view.js
@@ -21,8 +21,8 @@ class View {
         return video
     }
 
-    renderVideo({ userId, stream = null, url = null, isCurrentId = false}) {
-        const video = this.createVideoEl({ src: url, srcObject: stream })
+    renderVideo({ userId, stream = null, url = null, isCurrentId = false, muted = true }) {
+        const video = this.createVideoEl({ muted, src: url, srcObject: stream })
         this.appendToHTMLTree(userId, video, isCurrentId)
     }
 
@@ -37,6 +37,12 @@ class View {
 
         const videoGrid = document.getElementById('video-grid')
         videoGrid.append(div)
+    }
+
+    setParticipants(count) {
+        const myself = 1
+        const participants = document.getElementById('participants')
+        participants.innerHTML = count + myself
     }
 
 }
